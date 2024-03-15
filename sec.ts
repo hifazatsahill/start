c0f0f226c2ff7be3197ec51e75f0b7e15733887f
unknown <hifazatsahill@gmail.com>
2024-03-15 09:52:30 +0500

    sec

---
+++ sec.ts
@@ -0,0 +1,34 @@
+// // console.log("Hello world");
+let firstName ="Asgr";
+let age = 36;
+
+// let message = "Hello my name is: "+
+// firstName + "and my age is: "+age
+// tsc sec.ts && node sec.js ## ak sath open krny k liyaa
+// let message =`hello my name is:${firstName} and my age is:${age}`
+// let message = `Hello my name is:${firstName} and my age is: ${age}`;
+let index =10;
+
+// ++index ///index = 11 //Urinary 
+// --index//index =10
+// ++index//index =13
+// --index
+ // --index
+ //             11   +   12   +7+ 11      -2 = 39
+//  let result =++index+ ++index+ 7+ --index -2 
+// console.log(result);
+// Assigment Operator
+// let num1 = 23;
+// num1+= 5;
+// num1-=3;
+// console.log(num1);
+
+// comparison Operator
+
+//  ==
+
+let num1 =55;
+let num2 = 55;
+
+let result= num1 > num2
+console.log(result);
